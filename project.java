2d0bd61294fa4e76da14748521f6d72bf6695a82
denisov93 <a.denisov@campus.fct.unl.pt>
2021-06-04 23:38:25 +0100

    basic example

--- project.java
+++ project.java
@@ -1,44 +1,75 @@
 /*@
-predicate Positive(unit a, int v; unit b) = v >= 0 &*& b == unit;
+//predicate Positive(unit a, int v; unit b) = v >= 0 &*& b == unit;
 
-predicate Inv(Queue q;) = 
-	q.buffer |-> ?a
-	&*& q.n |-> ?v
-	&*& 0 <= v
-	&*& v <= a.length
-	&*& array_slice_deep(a,0,v,Positive,unit,_,_)
-	&*& array_slice(a,v,a.length,_);
+predicate QueueInv(Queue q; int n, int m) = 
+	q.array |-> ?a
+    &*& a != null
+    &*& array_slice(a,0,a.length,?elems)
+    &*& q.numelements |-> n
+    &*& m == a.length
+    &*& q.head |-> ?h
+    &*& q.tail |-> ?t
+    &*& 0 <= h &*& h < a.length
+    &*& 0 <= t &*& t < a.length
+    &*& (h==t? n==0 || n==a.length : true)
+    &*& (h>t? n==h-t : true)
+    &*& (h<t? n==h-t + a.length : true)
+    ; 
 @*/
 
 //Queue based on a circular buffer.
 class Queue {
-    int buffer[];
-    int n;
+    int[] array;
+    int numelements;
+    int head;
+    int tail;
   
     //creates a new Queue with capacity max.
-    Queue(int max){}
+    Queue(int size)
+    //@ requires size > 0;
+    //@ ensures QueueInv(this,0,size);
+    {
+        array = new int[size];
+        numelements = 0;
+        head = 0;
+        tail = 0;
+    }
   
     //places the int v at the end of this Queue
-    void enqueue(int x)
-    //@ requires Inv(this) &*& x >= 0;
-    //@ ensures Inv(this);
+    void enqueue(int v)
+    //@ requires QueueInv(this,?n,?m) &*& n < m;
+    //@ ensures QueueInv(this,n+1,m);
     {
-        if(n< buffer.length){
-            buffer[n++] = x;
-            //@ assert this.n |-> ?v &*& this.buffer |-> ?a;
-            //@ array_slice_deep_close(a,v-1,Positive,unit);
-            //@ array_slice_deep_join(a,0);
-            //@ assert array_slice_deep(a,0,v,Positive,unit,_,_);
-        }
+        array[head++] = v;
+        if(head == array.length) head = 0;
+        numelements++;
     }
   
     //retrieves the element at the start of this Queue.
-    int dequeue(){}
+    int dequeue()
+    //@ requires QueueInv(this,?n,?m) &*& n>0;
+    //@ ensures QueueInv(this,n-1,m);
+    {
+        int v = array[tail++];
+        numelements--;
+        if(tail==array.length) tail=0;
+        return v;
+    }
     
     //returns true if this Queue has reached its capacity.
-    boolean isFull(){}
+    boolean isFull()
+    //@ requires QueueInv(this,?n,?m);
+    //@ ensures QueueInv(this,n,m) &*& result == (n==m);
+    {
+        return numelements== array.length;
+    }
     
     //returns true if this Queue has no elements.
-    boolean isEmpty(){}
+    boolean isEmpty()
+    //@ requires QueueInv(this,?n,?m);
+    //@ ensures QueueInv(this,n,m) &*& result == (n==0);
+    {
+        return numelements == 0;
+    }
   
   }
