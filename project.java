97a4499ecd4ce99c712ba812858a3dc46cebeeda
Samuel Robalo <samuelrobalo2305@hotmail.com>
2021-06-05 04:13:15 +0100

    JesusChristUpdate

--- project.java
+++ project.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.locks.*;
+
 /*@
 predicate Valid(unit a, A o; unit b) = o != null &*& AInv(o) &*& b == unit;
 
@@ -97,3 +99,139 @@ class Queue {
     }
   
   }
+
+/*@
+predicate_ctor CQueueSharedState(CQueue c)(;) =
+        c.q |-> ?q &*& q != null &*& QueueInv(q, _, _);
+
+predicate_ctor CQueueSharedState_notempty(CQueue c)(;) =
+        c.q |-> ?q &*& q != null &*& QueueInv(q, ?n, _) &*& n > 0;
+
+predicate_ctor CQueueSharedState_notfull(CQueue c)(;) =
+        c.q |-> ?q &*& q != null &*& QueueInv(q, ?n, ?m) &*& n < m;
+
+predicate CQueueInv(CQueue c;) = 
+        c.mon |-> ?l
+        &*& l != null
+        &*& l lck(l,l,CQueueSharedState(c));
+
+        &*& c.notempty |-> ?c1
+        &*& c1 != null
+        &*& cond(c1, CQueueSharedState(c), CQueueSharedState_notempty(c))
+
+        &*& c.notfull |-> ?c2
+        &*& c2 != null
+        &*& cond(c2, CQueueSharedState(c), CQueueSharedState_notfull(c))
+@*/
+  class CQueue {
+    Queue q;
+    ReentrantLock mon;
+    Condition notempty;
+    Condition notfull;
+
+    CQueue(int size)
+    //@ requires size > 0;
+    //@ ensures CQueueInv(this);
+    {
+        q = new Queue(size);
+        //@ close CQueueSharedState(this)();
+        //@ close enter_lck(l, CQueueSharedState(this));
+        mon = new ReentrantLock();
+        //@ close set_cond(CQueueSharedState(this), CQueueSharedState_notempty(this));
+        notempty = mon.newCondition();
+        //@ close set_cond(CQueueSharedState(this), CQueueSharedState_notfull(this));
+        notfull = mon.newCondition();
+    }
+
+      void enqueue(A v)
+      //@ requires [?f]CQueueInv(this) &*& v != null &*& AInv(v);
+      //@ ensures [f]CQueueInv(this);
+      {
+          mon.lock();
+          if(!q.isFull()){
+                notfull.await();
+                //@ open CQueueSharedState_notfull(this)();
+          }
+          //@ open QueueInv(q,_,_);
+          q.enqueue(v);
+          notempty.signal();
+          mon.unlock();
+      }
+
+      A dequeue()
+      //@ requires [?f]CQueueInv(this);
+      //@ ensures [f]CQueueInv(this);
+      {
+          mon.lock();
+          if(!q.isEmpty()){
+                notempty.await();
+                //@ open CQueueSharedState_notempty(this)();
+          }
+          //@ open QueueInv(q,_,_)
+          A v = q.dequeue();
+          mon.unlock();
+          notfull.signal();
+          return v;
+      }
+  }
+
+/*@
+    predicate ProducerInv(Producer p;) = 
+        p.q |-> ?q
+        &*& q != null 
+        &*& [_]CQueueInv(q)
+        &*& p.id |-> ?v
+        &*& v >= 0
+@*/
+
+//@predicate fraq(real f) = true;
+
+class Producer implements Runnable{
+    CQueue q;
+    int id;
+
+    //@ predicate pre() = ProducerInv(this);
+    //@ predicate post() = emp;
+    public Producer(CQueue q, int id)
+    //@ requires q != null &*& frac(?f) &*& [f]CQueueInv(q) &*& id >= 0;
+    //@ ensures ProducerInv(this);
+    {
+        this.q = q;
+        this.id = id;
+    }
+
+    public void run(){
+        //@ requires pre();
+        //@ ensures post();
+        while(true)
+        //@ invariant ProducerInv(this);
+        {
+            q.enqueue(new A(id));
+        }
+    }
+}
+
+class Consumer{
+
+}
+
+class ProducerConsumer {
+
+    public static void main(String []args)
+    //@ requires true;
+    //@ ensures true;
+    {
+        CQueue q = new CQueue(10);
+        //@ assert CQueueInv(q);
+        //@ close frac(1);
+        for( int i = 0; i < 10; i++ )
+        //@ invariant 0 <= i &*& i <= 10 &*& frac(?f) &*& [f]CQueueInv(q);
+        {
+            //@open fraq(f);
+            //@close fraq(f/2);
+            new Thread(new Producer(q,i)).start();
+            //@close fraq(f/2);
+            new Thread(new Consumer(q)).start();
+        }
+    }
+}
